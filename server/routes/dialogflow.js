2476e03b2e18d71d2906c73020694bebc021bef5
taey1344 <xodus1344@gmail.com>
2020-09-30 00:23:27 +0900

    day_1

---
+++ server/routes/dialogflow.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const router = express.Router();
+const structjson = require('./structjson.js');
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+
+const config = require('../config/keys');
+
+const projectId = config.googleProjectID
+const sessionId = config.dialogFlowSessionID
+const languageCode = config.dialogFlowSessionLanguageCode
+
+
+// Create a new session
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+// We will make two routes 
+
+
+// Text Query Route
+
+router.post('/textQuery', async (req, res) => {
+    // Client에서 DialogFlow API로 가는 정보가 필요.
+    // dialogflow npm 에서 소스가져옴
+
+    // The text query request.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                // The query to send to the dialogflow agent
+                // 입력 값으로 받는 텍스트 받아옴
+                text: req.body.text,
+                // The language used by the client (en-US)
+                languageCode: languageCode,
+            },
+        },
+    };
+
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+
+    res.send(result);
+
+
+});
+
+
+//Event Query Route
+
+router.post('/eventQuery', async (req, res) => {
+    //We need to send some information that comes from the client to Dialogflow API 
+    // The text query request.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            event: {
+                // The query to send to the dialogflow agent
+                name: req.body.event,
+                // The language used by the client (en-US)
+                languageCode: languageCode,
+            },
+        },
+    };
+
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+
+    res.send(result)
+})
+
+
+
+
+
+
+
+module.exports = router;
